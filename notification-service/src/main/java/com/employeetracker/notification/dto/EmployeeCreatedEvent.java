f505c51c284ccb2099958e4668688d0f6d849f49
Saravana20-S <iamsaravanan1347@gmail.com>
2026-09-01 12:05:42 +0530

    Added DTO logic for notification

---
+++ notification-service/src/main/java/com/employeetracker/notification/dto/EmployeeCreatedEvent.java
@@ -0,0 +1,21 @@
+package com.employeetracker.notification.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmployeeCreatedEvent {
+
+    private String employeeId;
+
+    private String name;
+
+    private String email;
+
+    private String department;
+
+    private String eventType;
+}
